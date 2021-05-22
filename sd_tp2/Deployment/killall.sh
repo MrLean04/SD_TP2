636ff0d1ed5b3f2a32813c0d93e67e67246e9f14
MrLean04 <55977102+MrLean04@users.noreply.github.com>
2021-05-22 16:22:25 +0100

    upd sh

--- sd_tp2/Deployment/killall.sh
+++ sd_tp2/Deployment/killall.sh
@@ -6,7 +6,7 @@ sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws06.ua.pt "
 sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws07.ua.pt "/usr/sbin/fuser -n java -k 22444"
 
 
-sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws04.ua.pt "/usr/sbin/fuser -n java-k 22440"
+sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws04.ua.pt "/usr/sbin/fuser -n java -k 22440"
 
 sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws01.ua.pt "/usr/sbin/fuser -n java -k 22441"
 sshpass -p qwerty ssh -tt -o StrictHostKeyChecking=no sd405@l040101-ws02.ua.pt "/usr/sbin/fuser -n java -k 22442"
